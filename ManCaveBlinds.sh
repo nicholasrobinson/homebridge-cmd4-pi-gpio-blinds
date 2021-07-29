7eabb1aaf88fb289b767c82eb77b546e620fb85b
Nicholas Robinson <nicholas.s.robinson@gmail.com>
2021-07-29 15:45:23 -0700

    Simplifying script

--- ManCaveBlinds.sh
+++ ManCaveBlinds.sh
@@ -3,18 +3,26 @@
 # Script for controlling blinds using GPIO Zero.
 # Usage: bash ManCaveBlinds.sh Set ManCaveBlinds TargetPosition 100 
 
+if [ "$1" = "Get" ]; then
+    if [ "$3" = "Name" ]; then
+        echo "$2"
+        exit 0
+    fi
+fi
+
 if [ "$1" = "Set" ]; then
    if [ "$3" = "TargetPosition" ]; then
       if [ "$4" = "100" ]; then
          # Open blinds
-         /var/lib/homebridge/Cmd4Scripts/man_cave_blinds.py open 
+         /var/lib/homebridge/Cmd4Scripts/man_cave_blinds.py open & 
       else
          # Close blinds
-         /var/lib/homebridge/Cmd4Scripts/man_cave_blinds.py close 
+         /var/lib/homebridge/Cmd4Scripts/man_cave_blinds.py close & 
       fi
    fi
 fi
 
-# Update state
-echo 0
-exit 0
+STATE_OUTPUT=`/var/lib/homebridge/Cmd4Scripts/State.js $1 $2 $3 $4`
+EXIT_CODE=$?
+echo "$STATE_OUTPUT" 
+exit $EXIT_CODE
